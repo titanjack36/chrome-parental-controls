3926660a519c266fc314b2bdbbcbec6a676febc7
titanjack36 <titanjack36@users.noreply.github.com>
2020-07-04 14:16:24 -0400

    fix(redirect-page): added multiple backgrounds and changed styling

--- redirect-page.js
+++ redirect-page.js
@@ -1,3 +1,17 @@
+var backgroundImages = [
+  "background1.jpg",
+  "background2.jpg",
+  "background3.jpg",
+  "background4.jpg",
+  "background5.jpg",
+];
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  let randIndex = Math.floor(Math.random() * 5);
+  let body = document.getElementsByTagName('body')[0];
+  body.style.backgroundImage = "url(assets/" + backgroundImages[randIndex] + ")";
+});
+
 var port = chrome.runtime.connect({ name: "timer" });
 var interval = setInterval(function () {
   port.postMessage({ action: 'getTime' });
